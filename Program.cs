46695e2a308f055b14f90a7c9abcf768c12109b8
DullishBird <dullishbird@icloud.com>
2022-04-09 20:36:16 +0500

    i just want this to work

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
